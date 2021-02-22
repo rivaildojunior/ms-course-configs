49684dd04eb83f32d467b0dbb091ab4589099316
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2021-02-22 20:54:49 -0300

    Primeiro commit

---
+++ src/main/java/com/example/funcionarioservice/FuncionarioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.funcionarioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/funcionarioservice/FuncionarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funcionarioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
